36902c3c2116fd5d1fe5425b55387b100ef83330
HarmanKaur2001 <200456178@student.georgianc.on.ca>
2021-02-27 13:47:49 -0500

    add the GUI and modify the functions in it as well as add fxml

--- src/controller/StudentViewController.java
+++ src/controller/StudentViewController.java
@@ -1,9 +1,13 @@
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
-public class StudentViewController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class StudentViewController implements Initializable {
     @FXML
     private Label firstNameLabel;
 
@@ -14,4 +18,10 @@ public class StudentViewController {
     private Label studentIdLabel;
 
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        firstNameLabel.setText("Harman");
+        lastNameLabel.setText("Kaur");
+        studentIdLabel.setText("200456178");
+    }
 }
