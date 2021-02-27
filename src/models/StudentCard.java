f09f7529002f9eb54dd35d614ee0d5f9ce1d881c
HarmanKaur2001 <200456178@student.georgianc.on.ca>
2021-02-26 19:31:18 -0500

    add the student card and student interest class as well as MVC (Models, controller and view) is added.

--- src/models/StudentCard.java
+++ src/models/StudentCard.java
@@ -1,4 +1,118 @@
 package models;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.List;
+/*purpose of the class: to add first name, last name, student id and interests
+ *name: harman kaur
+ * date:2021-02-09
+ * time:10:30 am
+ */
 public class StudentCard {
+    /**
+     * test commit test here1
+     */
+    private String firstName;//firstname is taken as string
+    private String lastName;//last name is taken as string
+    private int studentId;//studentId is taken as integer
+    private ArrayList<String> interests;//interests are taken as array list type string
+
+    public StudentCard(String firstName, String lastName, int studentId, ArrayList<String> interests)
+    {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setStudentId(studentId);
+        setInterests(interests);
+    }
+
+
+    //here the getter and setter method is put for first name, last name, student id and interests
+    public String getFirstName() {
+        return firstName;
+    }
+
+    //first names are put under array list type string
+    public static List<String> getFirstNames() {
+        return Arrays.asList("Harman", "Sukhman", "Agam", "Gurnoor", "Simran", "Harpal", "Rupinder");
+    }
+
+    public void setFirstName(String firstName) {
+        //firstName = firstName.toLowerCase();//this is case sensitive case
+        List<String> firstNames = getFirstNames();
+        //here the validation of the first name
+        if (firstNames.contains(firstName))
+            this.firstName = firstName;
+        else
+            throw new IllegalArgumentException(firstName + "is not valid. chose from these"+ firstNames);
+    }
+
+    public void setLastName(String lastName){
+        lastName = lastName.toLowerCase();
+        //here the last name is to be validated
+        if (lastName.contains(lastName))
+            this.lastName= lastName;
+
+        else
+            throw new IllegalArgumentException(lastName + "has no rule to use upper case" + lastName);
+    }
+
+    public void setStudentId(int studentId){
+        //student id is to be validated
+        if (studentId>=10000)
+            this.studentId = studentId;
+        else
+            throw new InputMismatchException(studentId +"has no chars"+ studentId);
+    }
+
+    //getter for the last name
+    public String getLastName() {
+        return lastName;
+    }
+
+    //getter for the student id
+    public int getStudentId() {
+        return studentId;
+    }
+
+    //getter for the interests
+    public ArrayList<String> getInterests() {
+        return interests;
+    }
+
+    //interests are write under array list
+    public void setInterests(ArrayList<String> interests) {
+        List<String> validInterests = Arrays.asList("riding","movies","travelling","skiing","dancing","gaming","reading");
+        for (int i =0;i<interests.size();i++) {
+            if (validInterests.contains(interests.get(i)))//case sensitive comparison
+                this.interests = interests;
+            else
+                throw new IllegalArgumentException(interests + " " + "is not valid. use one of them" + validInterests);
+        }
+    }
+
+    //here the theme for the interests are used where we get to know that interests are horrific or interesting
+    public String getTheme()
+    {
+        if(interests.equals("riding")|| interests.equals("skiing"))
+            return " horrific";
+        else
+            return "interesting";
+    }
+
+    //the index for the first name is get here
+    public int getFirstIndex()
+    {
+        //index of each interests
+        return getFirstNames().indexOf(firstName) + 2;
+    }
+
+    //string method is used for the return the interests of the first name and of is used as concatenated
+    public String toString()
+    {
+        return interests +" " +"of" +" "+ firstName;
+    }
 }
+
+
--- src/models/StudentInterests.java
+++ src/models/StudentInterests.java
@@ -1,4 +1,41 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*
+ *purpose:to test the student class
+ * name:Harman Kaur
+ * time:12:00pm
+ * date: 2021
+ */
+
 public class StudentInterests {
+    public static void main(String[] args) {
+        Scanner key = new Scanner(System.in);//here the scanner is used
+
+        //arraylist of unchartedInterests are created
+        ArrayList<String> unchartedInterests = new ArrayList<>();
+        unchartedInterests.addAll(Arrays.asList("riding","movies","travelling"));
+        StudentCard interestsOfStudent = new StudentCard("Harman","Kaur",67868,unchartedInterests);
+        StudentCard otherInterests =  new StudentCard("Sukhman","kaur",767868,unchartedInterests);
+
+        //now the first name, last name, student id and interests are printed
+        System.out.println("First Name:" + interestsOfStudent.getFirstName());
+        System.out.println("last Name:"  + interestsOfStudent.getLastName());
+        System.out.println("Student ID:" + interestsOfStudent.getStudentId());
+        System.out.println("Interests:"  + interestsOfStudent.getInterests());
+
+        //otherStudent.setInterests("riding");//valid input
+        //otherStudent.setInterests("Dancing");//invalid input
+
+
+        System.out.printf("%s is a %s hobby with the value of %d  %n" ,interestsOfStudent,interestsOfStudent.getTheme()
+                ,interestsOfStudent.getFirstIndex(),interestsOfStudent.getLastName());
+
+        System.out.printf("%s is a %s hobby with the value of %d %n" ,otherInterests,otherInterests.getTheme()
+                ,otherInterests.getFirstIndex());
+    }
 }
+
