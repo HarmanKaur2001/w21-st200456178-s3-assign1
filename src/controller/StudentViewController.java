5179b79d2011f22cdbe0b130577630c9cd4e2a7e
HarmanKaur2001 <200456178@student.georgianc.on.ca>
2021-03-09 22:20:21 -0500

    the list view is added

--- src/controller/StudentViewController.java
+++ src/controller/StudentViewController.java
@@ -1,10 +1,13 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ScrollBar;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import models.StudentCard;
@@ -12,6 +15,8 @@ import models.StudentCard;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+
+import static javafx.scene.control.SelectionMode.*;
 
 public class StudentViewController implements Initializable {
     @FXML
@@ -30,18 +35,24 @@ public class StudentViewController implements Initializable {
     private ImageView imageView;
     @FXML
     private ScrollBar scrollBar;
+    @FXML
+    private ListView<String> listView;
+    ObservableList<String> list = FXCollections.observableArrayList("Hiking","riding","riding","movies","travelling","skiing","dancing","gaming","reading");
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         ArrayList<String> unchartedInterests = new ArrayList<>();
+        ListView listViewRef=new ListView();
+
         StudentCard studentCard = new StudentCard("Harman", "Kaur", 200456178, unchartedInterests);
         firstNameLabel.setText(studentCard.getFirstName());
         lastNameLabel.setText(studentCard.getLastName());
         studentIdLabel.setText(Integer.toString(studentCard.getStudentId()));
        // favoriteActivitiesLabel.getLabelFor(studentCard.getInterests());
         imageView.setImage(studentCard.getStudentImage());
-        scrollBar.adjustValue(10);
+        listView.setItems(list);
+        listView.getSelectionModel().setSelectionMode(MULTIPLE);
 
     }
 }
